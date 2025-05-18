e4a7a136c25a35704254ee6756fb19f9e9351caa
temp638089 <temp638089@gmail.com>
2025-05-18 09:12:33 +0530

    Media commit

--- myworld/settings.py
+++ myworld/settings.py
@@ -2,23 +2,13 @@ from pathlib import Path
 import os
 import dj_database_url
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.1.25','django-project-wbi4.onrender.com']
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -63,17 +53,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'myworld.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -90,10 +72,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/5.1/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -102,16 +80,9 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
@@ -128,7 +99,7 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = True  # Logs out user when browser is closed
 #SESSION_COOKIE_AGE = 60  # Auto logout after 5 minutes of inactivity
 #SESSION_SAVE_EVERY_REQUEST = True  # Refresh session expiration on every request
 
-#MEDIA_URL='/media/'
+MEDIA_URL='/media/'
 #MEDIA_ROOT=BASE_DIR / 'media'
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
--- student/admin.py
+++ student/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import *
-# Register your models here.
-admin.site.register(student)
+from .models import student
+
+@admin.register(student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'deaprtment', 'grade', 'mark')
+    fields = ('name', 'email', 'deaprtment', 'grade', 'mark', 'image')  # <-- Include 'image'
